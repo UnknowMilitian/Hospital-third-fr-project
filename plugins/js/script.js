fb4e983e765f2b4872013edddef0f0854917f20b
UnknowMilitian <j.umariy4238@gmail.com>
2023-06-17 19:43:47 +0500

    Finish of navbar section

--- plugins/js/script.js
+++ plugins/js/script.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+// ! Navbar Scripts
+const navItems = document.querySelector(".nav__items");
+const openNavBtn = document.querySelector(".open__nav-btn");
+const closeNavBtn = document.querySelector(".close__nav-btn");
+
+openNavBtn.addEventListener("click", () => {
+  navItems.style.display = "flex";
+  openNavBtn.style.display = "none";
+  closeNavBtn.style.display = "inline-block";
+});
+
+closeNavBtn.addEventListener("click", () => {
+  navItems.style.display = "none";
+  openNavBtn.style.display = "inline-block";
+  closeNavBtn.style.display = "none";
+});
